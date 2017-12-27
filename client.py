d79f7cbeadbbf70d2c53c47d03f5cfbfbbfbfb90
Fyrngarm <erikthomas146@gmail.com>
2017-12-27 21:08:12 +0000

    Add files via upload

--- client.py
+++ client.py
@@ -10,9 +10,12 @@ class Client(Server):
         self.server_add = ('localhost', 10000)
 
     def send(self, message):
+        """Handles the sending of messages"""
         self.sck.sendto(message.encode(), self.server_add)
 
     def logic(self):
+        """Handles the logic for the client, including sending and receiving messages"""
+        #Loops whilst the connection is active
         while True:
             try:
                 """received = 0
@@ -23,41 +26,62 @@ class Client(Server):
                 #The data needs to be decoded, otherwise it will use the bytes-like format it was sent in
                 #It is also cast to a string to be used in the conditional statements
                 data = str(data.decode())
-                print("data is: ", data)
+                #print("data is: ", data)
                 #print("reached if statements")
                 if data == "not":
+                    #If the server says it's not the client's turn, sets current turn as false
                     currentTurn = False
-                    print("Server said no: ", currentTurn)
+                    #print("Server said no: ", currentTurn)
 
                 elif data == "yes":
+                    #If the server says it is the client's turn, sets current turn to true
                     currentTurn = True
-                    print("Server said yes: ", currentTurn)
+                    #print("Server said yes: ", currentTurn)
                 # received += len(currentTurn)
 
+                elif data == "difficulty":
+                    #If the client goes first, it is allowed to set the difficulty
+                    difficulty = input("Please type which difficulty you want:\n>Hard\n>Easy\n")
+                    #Converting to lower case is used for consistency in conditional statements
+                    difficulty = difficulty.lower()
+                    self.send(difficulty)
+
+
                 elif data == "Do you want to play again? Y/N":
+                    #Handles if the game is over
                     game_over = input("Game over! Do you want to play again? Y/N\n")
+                    #.lower() used again for consistency
                     game_over = str(game_over.lower())
+                    #If the user does not want to continue, the connection closes
                     if game_over == "n":
                         #If the user does not want to play again, closes the connection
                         print("Thank you for playing! Closing the connection...")
                         self.sck.close()
                         break
-
+                    #If the user wants to continue, the game is reset in the server
                     elif game_over == "y":
                         #If the user does want to play again, gets the server to re-initialise the game
                         print("Restarting game...")
                         self.send(game_over)
+                #Handles the game logic for if it is the client's turn
+                if currentTurn or data == "redo":
+                    if data == "redo":
+                        print("Please re-enter the value, the last one was invalid")
 
-                if currentTurn:
-                    print("current turn is true, reached if statement")
+                    #print("current turn is true, reached if statement")
+                    #remain = self.sck.recv(1024)
+                    print("Your turn... Standing by")
+                    #print("There are %d marbles remaining" % remain)
                     message = input("Please enter how many marbles you want to remove\n")
                     print("sending '%s'" % message)
                     # sck.sendto(message.encode(), server_add)
+                    #Sends how many marbles the client wants to remove to the server for it to calculate
                     self.send(message)
                     currentTurn = False
                     print("Current turn is now: ", currentTurn)
                     # print(sys.stderr, "received '%s'" % data)
                 else:
+                    #Waits for the server to make its turn rather than constantly checking
                     print("Waiting for server's turn... Waiting for 1 seconds...")
                     # Waits patiently for the server's turn
                     time.sleep(1)
@@ -66,6 +90,7 @@ class Client(Server):
 
             except Exception as e:
                 #If the game is over or something goes horribly wrong, closes the connection
+                #If an error is thrown, closes the socket for safety
                 self.sck.close()
                 #print("Exception thrown in client.py: ", e)
 
--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 import sys
+import math
+import time
 from random import randint
 
 class Server():
@@ -11,26 +13,10 @@ class Server():
         self.remain = 0
         #Stores the currently connected clients
         self.clients = []
+        self.buffer = 1024
+        self.num_check = False
 
-    def game_init_one_player(self):
-        print("hello")
-
-    def game_over(self, con):
-        game_over = "Do you want to play again? Y/N"
-        con.sendall(game_over.encode())
-        # game_over = str(game_over.lower())
-        data = con.recv(1024)
-        data = str(data.lower())
-
-        if data == "b'y'":
-            print("Reinitialising the game")
-            self.game_init_two_player()
-
-        elif data == "b'n'":
-            print("Game over, connection closing!\n")
-            con.close()
-
-    def game_init_two_player(self):
+    def game_init(self, con):
         """Initialises the game for two players, setting the difficulty and which player goes first"""
         #Generates a random number to decide which player, player 1 or player 2, goes first
         turn = randint(0,1)
@@ -38,20 +24,28 @@ class Server():
         if turn == 0:
             #Server first turn
             firstTurn = True
+            difficulty = input("Please type which difficulty you want:\n>Hard\n>Easy\n")
+            difficulty = difficulty.lower()
             print("Server first!")
         #If the number returned is one, the client goes first
         elif turn == 1:
             #Client first turn
             firstTurn = False
+            #Sends a message to the client, letting it know it can set the difficulty
+            message = "difficulty"
+            con.sendall(message.encode())
+            #Receives and decodes the client's choice, which will then be used to set the difficulty
+            difficulty = con.recv(self.buffer)
+            difficulty = str(difficulty.decode())
             print("Client first!")
-        #Alter this to allow client to set difficulty
-        difficulty = input("Please type which difficulty you want:\n>Hard\n>Easy\n")
-        difficulty = difficulty.lower()
+
         if difficulty == "hard":
+            #Generates a random value between 2 and 100 to set the initial marble stack as
             self.remain = randint(2,100)
             print("Hard selected: %d remaining" % self.remain)
 
         elif difficulty == "easy":
+            #Generates a random value between 2 and 20 to set the initial marble stack as
             self.remain = randint(2,20)
             print("Easy selected: %d remaining" % self.remain)
 
@@ -59,96 +53,170 @@ class Server():
 
     def logic(self, input):
         """This handles the logic for the program, this can be expanded to include AI"""
-        self.remain -= int(input)
-        print("%d marbles remaining..." % self.remain)
+        #Decrements the current remaining marbles by the given amount
+        print("Input: ", input, "\nRemaining: ", self.remain)
+        if int((self.remain / 2)) >= int(input):
+            self.remain -= int(input)
+            print("%d marbles remaining..." % self.remain)
+            print("Input: ", input, "\nNow Remaining: ", self.remain)
+            self.num_check = False
+
+        elif int(input) == 1 and int(self.remain) == 1:
+            print("One remaining...")
+            self.remain -= int(input)
+            self.num_check = False
+
+        else:
+            #print("Invalid value received, please enter a value less than half of the remaining marbles")
+            self.num_check = True
 
     def send(self, message, client):
+        """Used to send messages (legacy?)"""
         msg = str(message)
         self.sck.sendto(msg.encode(), client)
 
+    def receive(self, con):
+        """Handles the receiving of data packets, checking the entire packet has been received"""
+        print("end")
 
-    def game_two_players(self):
+    def turn_order(self, con, currentTurn):
+        while True:
+            # Checks to see if there are marbles remaining
+            if self.remain > 0:
+                if currentTurn:
+                    # Client turn
+                    # Sends this message as a confirmation to the client that it is its turn
+                    message = "yes"
+                    con.sendall(message.encode())
+                    print(">client current turn...")
+                    #time.sleep(2)
+                    #con.sendall(str(self.remain).encode())
+                    # Receives any values the client entered and decodes it
+                    data = con.recv(self.buffer)
+                    data = data.decode()
+                    print("received '%s'" % data)
+
+                    if data:
+                        print("sending data back to the client")
+                        # Calculates the new remaining value
+                        self.logic(int(data))
+                        print(str(self.num_check))
+                        currentTurn = False
+                        if self.num_check:
+                            #If the value is not valid, asks for another input from the client
+                            message = "redo"
+                            con.sendall(message.encode())
+                            data = con.recv(self.buffer)
+                            data = data.decode()
+                            self.logic(int(data))
+                            currentTurn = False
+
+                        else:
+                            currentTurn = False
+                            message = "There are %d marbles left!" % self.remain
+                            # Fixes when server is first, since client never knew when it was its turn
+                            con.sendall(message.encode())
+                            print("%d marbles remaining..." % self.remain)
+
+                        # con.sendall(data)
+                    else:
+                        print("no more data from client")
+                        break
+
+                elif not currentTurn:
+                    # Server turn
+                    print(">server current turn...")
+                    print("Sending current turn to client")
+                    message = "not"
+                    con.sendall(message.encode())
+                    print("server's turn now")
+                    message = input("It's your turn! Please enter the number of marbles you wish to remove:\n")
+                    self.logic(message)
+
+                    if self.num_check:
+                        message = input("Please re-enter a valid value:\n")
+                        self.logic(message)
+                        currentTurn = True
+
+                    elif not self.num_check:
+                        print("number valid?")
+                        print("%d marbles remaining..." % self.remain)
+                        currentTurn = True
+                    # con.sendall(str(self.remain).encode())
+            # If marbles has reached zero, runs the game over method
+            elif self.remain == 0:
+                self.game_over(con)
+            # Else, sets game over anyway...
+            else:
+                self.game_over(con)
+
+    def main_game(self):
         """Main logic for the two player version of the game, takes the socket as a parameter to allow for message sending and receiving"""
         # Game loop
-        while True:  # Change to remain > 0
+        while True:
+            #Waits for a client to connect before accepting a connection
             print(sys.stderr, "waiting for a connection")
             con, client_add = self.sck.accept()
             print("connection from", client_add)
-
-            firstTurn = self.game_init_two_player()
+            #Initialises the game once the client connects
+            firstTurn = self.game_init(con)
             if firstTurn:
+                #Server has first turn
                 print(">server first turn")
                 message = input("It's your turn first! Please enter how many marbles you wish to remove:\n")
-                self.remain -= int(message)
-                message = "The server just removed %d marbles!" % self.remain
+                self.logic(message)
+
+                if self.num_check:
+                    message = input("Please re-enter a valid value:\n")
+                    self.logic(message)
+
+                elif not self.num_check:
+                    message = "The server just removed %d marbles!" % self.remain
+                    # Fixes when server is first, since client never knew when it was its turn
+                    con.sendall(message.encode())
+                    print("%d marbles remaining..." % self.remain)
+
+            elif not firstTurn:
+                #Client has first turn
+                # send(firstTurn, sck, server_add)
+                print(">client first turn")
+                print("Waiting for 2 seconds to initialise client connection...")
+                #Having this delay ensures that the separate messages for the client's turn status
+                #and the no. of marbles remaining are kept apart to prevent the client stalling
+                time.sleep(2)
+                message = "Difficulty set. %d marbles remaining!" % self.remain
                 # Fixes when server is first, since client never knew when it was its turn
                 con.sendall(message.encode())
                 print("%d marbles remaining..." % self.remain)
-            elif not firstTurn:
-                # send(firstTurn, sck, server_add)
-                print(">client first turn")
-                msg = "hello"
-                con.sendall(msg.encode())
+
+
             try:
                 # print("connection from", client_add)
+                #Inverts the current value of the first turn to get the initial value for the second turn
                 currentTurn = ~firstTurn
+                self.turn_order(con, currentTurn)
 
-                while True:
-                    # Change this value to change no. of bytes received
-                    if self.remain > 0:
-                        if currentTurn:
-                            # Client turn
-                            message = "yes"
-                            con.sendall(message.encode())
-                            print(">client current turn...")
-                            data = con.recv(1)
-                            print("received '%s'" % data)
-                            if data:
-                                print("sending data back to the client")
-                                # Calculates the new remaining value
-                                self.logic(int(data))
-                                currentTurn = False
-                                # con.sendall(data)
-                            else:
-                                print("no more data from", client_add)
-                                break
-
-                        elif not currentTurn:
-                            # Server turn
-                            print(">server current turn...")
-                            print("Sending current turn to client")
-                            message = "not"
-                            con.sendall(message.encode())
-                            print("server's turn now")
-                            message = input("It's your turn! Please enter the number of marbles you wish to remove:\n")
-                            self.logic(message)
-                            print("%d marbles remaining..." % self.remain)
-                            #con.sendall(str(self.remain).encode())
-                            currentTurn = True
-
-                    elif self.remain == 0:
-                        self.game_over(con)
-                        """print("Remain has reached %d" % remain)
-                        game_over = "Do you want to play again? Y/N"
-                        con.sendall(game_over.encode())
-                        # game_over = str(game_over.lower())
-                        data = con.recv(1024)
-                        data = str(data.lower())
-
-                        if data == "b'y'":
-                            print("Reinitialising the game")
-                            self.game_init_two_player()
-
-                        elif data == "b'n'":
-                            print("Game over, connection closing!\n")
-                            con.close()"""
-
-                    else:
-                        self.game_over(con)
-
+            #Can be changed to handle specific exceptions
             except Exception as e:
                 print("Exception thrown in two player connection: ", e)
+                con.close()
 
-            """finally:
-                # Closes the connection to clean up
-                con.close()"""
+    def game_over(self, con):
+        """Handles the game over state, checking if the client wants to play again or not"""
+        game_over = "Do you want to play again? Y/N"
+        con.sendall(game_over.encode())
+        # game_over = str(game_over.lower())
+        #Receives the user's choice of game over state
+        #The data is decoded to convert to a true string type
+        data = con.recv(self.buffer)
+        data = str(data.lower().decode())
+
+        if data == "y":
+            #Resets the game if specified by the user
+            print("Reinitialising the game")
+            self.game_init(con)
+
+        elif data == "n":
+            #Else, ends the connection with the client if they want to quit
+            print("Game over, connection closing!\n")
+            con.close()
